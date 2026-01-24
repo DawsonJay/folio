bbc5c4c5f365c5f4faf4ed07b0866cc6e42eeb5b
James <james@example.com>
2026-01-24 07:30:03 +0000

    Update frontend dependencies and remove Tailwind CSS configuration
    - Added `react-router-dom` for routing functionality.
    - Replaced Tailwind CSS with SASS for styling, removing related configuration files.
    - Updated `vite.config.ts` to disable PostCSS.
    - Cleaned up `App.tsx` to implement routing with a new `LandingPage` component.

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,10 @@
+import './../styles/pages/_landing-page.scss';
+
+export default function LandingPage() {
+  return (
+    <div className="landing-page">
+      <nav></nav>
+      <div className="content"></div>
+    </div>
+  );
+}
